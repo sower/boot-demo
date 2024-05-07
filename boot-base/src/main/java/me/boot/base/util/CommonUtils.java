bc467236e561c3a57a231481b999db1cab8fcc76
Ylem <remarkablerain@126.com>
2024-05-07 22:38:42 +0800

    feat: Diff Collection

--- boot-base/src/main/java/me/boot/base/util/CommonUtils.java
+++ boot-base/src/main/java/me/boot/base/util/CommonUtils.java
@@ -1,11 +1,15 @@
 package me.boot.base.util;
 
+import java.beans.BeanInfo;
+import java.beans.Introspector;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
@@ -23,18 +27,37 @@ public class CommonUtils {
         PathMatchingResourcePatternResolver resourcePatternResolver = new PathMatchingResourcePatternResolver();
         Resource[] resources = resourcePatternResolver.getResources(
             ResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX + "me/boot/**/*.class");
-        return Arrays.stream(resources).map(
-            resource -> {
-                try {
-                    return new SimpleMetadataReaderFactory().getMetadataReader(resource)
-                        .getClassMetadata()
-                        .getClassName();
-                } catch (IOException e) {
-                    log.warn("Failed to get class for resource {}", resource, e);
-                }
-                return null;
+        return Arrays.stream(resources).map(resource -> {
+            try {
+                return new SimpleMetadataReaderFactory().getMetadataReader(resource)
+                    .getClassMetadata().getClassName();
+            } catch (IOException e) {
+                log.warn("Failed to get class for resource {}", resource, e);
             }
-        ).filter(StringUtils::isNotBlank).collect(Collectors.toList());
+            return null;
+        }).filter(StringUtils::isNotBlank).collect(Collectors.toList());
+    }
+
+    public static boolean isJavaBean(Class<?> clazz) {
+        if (BeanUtils.isSimpleProperty(clazz)) {
+            return false;
+        }
+        try {
+            BeanInfo beanInfo = Introspector.getBeanInfo(clazz, Object.class);
+            return ArrayUtils.isNotEmpty(beanInfo.getPropertyDescriptors());
+        } catch (Exception e) {
+            log.warn("Failed to get bean info for class {}", clazz, e);
+        }
+        return false;
+    }
+
+    public static boolean isAllJavaBean(Object... bean) {
+        for (Object object : bean) {
+            if (!isJavaBean(object.getClass())) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
--- boot-base/src/main/java/me/boot/base/util/ObjectDiffUtils.java
+++ boot-base/src/main/java/me/boot/base/util/ObjectDiffUtils.java
@@ -1,17 +1,25 @@
 package me.boot.base.util;
 
 import com.alibaba.fastjson2.JSONObject;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.MapDifference;
 import com.google.common.collect.MapDifference.ValueDifference;
 import com.google.common.collect.Maps;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 import java.util.stream.Collectors;
+import lombok.SneakyThrows;
 import me.boot.base.dto.FiledDifference;
 import org.apache.commons.lang3.builder.ReflectionDiffBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.lang.NonNull;
 
 /**
@@ -49,6 +57,9 @@ public abstract class ObjectDiffUtils {
 
     public static List<FiledDifference> diff(@NonNull Object before, @NonNull Object after,
         Collection<String> ignoreProperties) {
+        if (!CommonUtils.isAllJavaBean(before, after)) {
+            throw new IllegalArgumentException("The two objects must be JavaBean");
+        }
         // Map<String, ?> beforeMap = (Map<String, ?>) BeanMap.create(before);
         // Map<String, ?> afterMap = (Map<String, ?>) BeanMap.create(after);
 
@@ -72,6 +83,67 @@ public abstract class ObjectDiffUtils {
         return entriesDiffering.entrySet().stream().map(
             entry -> new FiledDifference(entry.getKey(), entry.getValue().leftValue(),
                 entry.getValue().rightValue())).collect(Collectors.toList());
+    }
+
+
+    /**
+     * 同类对象集合差异比对
+     *
+     * @param before 旧值
+     * @param after  新值
+     * @return 差异列表对
+     */
+    public static <T> Pair<List<Object>, List<Object>> diffList(@NonNull Collection<T> before,
+        @NonNull Collection<T> after, String key) {
+        // 以相同标识对比
+        ImmutableMap<Object, T> beforeMap = Maps.uniqueIndex(before,
+            item -> getIdentifier(key, item));
+        ImmutableMap<Object, T> afterMap = Maps.uniqueIndex(after,
+            item -> getIdentifier(key, item));
+        MapDifference<Object, Object> difference = Maps.difference(beforeMap, afterMap);
+
+        List<Object> oldList = new LinkedList<>();
+        List<Object> newList = new LinkedList<>();
+        difference.entriesDiffering().entrySet().stream().map(entry -> buildDiffPair(key, entry))
+            .forEach(pair -> {
+                oldList.add(pair.getLeft());
+                newList.add(pair.getRight());
+            });
+        oldList.addAll(difference.entriesOnlyOnLeft().values());
+        newList.addAll(difference.entriesOnlyOnRight().values());
+        return Pair.of(oldList, newList);
+    }
+
+    private static Pair<Map<String, Object>, Map<String, Object>> buildDiffPair(String key,
+        Entry<Object, ValueDifference<Object>> entry) {
+        ValueDifference<Object> value = entry.getValue();
+        Pair<Map<String, Object>, Map<String, Object>> diffPair = getDiffPair(value.leftValue(),
+            value.rightValue());
+        // 反填标识符
+        Map<String, Object> left = new HashMap<>(diffPair.getLeft());
+        left.put(key, entry.getKey());
+        Map<String, Object> right = new HashMap<>(diffPair.getRight());
+        right.put(key, entry.getKey());
+        return Pair.of(left, right);
+    }
+
+    @SneakyThrows
+    private static <T> Object getIdentifier(String key, T item) {
+        return FieldUtils.readField(item, key, true);
+    }
+
+    private static Pair<Map<String, Object>, Map<String, Object>> getDiffPair(
+        @NonNull Object before, @NonNull Object after) {
+        if (!CommonUtils.isAllJavaBean(before, after)) {
+            throw new IllegalArgumentException("The two objects must be JavaBean");
+        }
+        Map<String, Object> beforeMap = JSONObject.from(before);
+        Map<String, Object> afterMap = JSONObject.from(after);
+        MapDifference<String, Object> difference = Maps.difference(beforeMap, afterMap);
+        Set<String> properties = difference.entriesDiffering().keySet();
+        beforeMap = Maps.filterKeys(beforeMap, properties::contains);
+        afterMap = Maps.filterKeys(afterMap, properties::contains);
+        return Pair.of(beforeMap, afterMap);
     }
 
 }
